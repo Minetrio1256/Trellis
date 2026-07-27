4bf4811e5afac35c2d0d9d65280657a9d6e8d6d6
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2026-07-27 11:05:56 -0400

    fixed wrong path to lib x3

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { auth } from "../middleware/auth.js";
-import permission from "../middleware/permission.js";
+import permission from "../services/permission.js";
 import Permission from "../permissions.js";
 
 import {
